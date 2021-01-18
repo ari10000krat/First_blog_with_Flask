eab48dbdd183fabf86ce73f8f58e264468f6aded
glebPC <ksendziuk.gleb@gmail.com>
2021-01-18 14:38:15 +0200

    Add Database

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
 
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def __repr__(self):
+        return f'<Article {self.id}>'
 
 @app.route('/')
 def index():
@@ -11,7 +26,6 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 
 @app.route('/user/<string:name>/<int:id>')
