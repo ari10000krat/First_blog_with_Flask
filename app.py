3d1715eae7edd8ef65b94afca2bdf497f54123c2
glebPC <ksendziuk.gleb@gmail.com>
2021-01-18 15:09:58 +0200

    Add connection with database from page "create-article.html"

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
@@ -14,9 +16,9 @@ class Article(db.Model):
     text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
-
     def __repr__(self):
         return f'<Article {self.id}>'
+
 
 @app.route('/')
 def index():
@@ -28,9 +30,22 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return f'User page: {name} - {id}'
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title,intro=intro,text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'При добавлении статьи произошла ошибка'
+    else:
+        return render_template('create-article.html')
 
 
 if __name__ == '__main__':
